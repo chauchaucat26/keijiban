8840ce70e03349bf2ef06cb9331ac188b684f2ce
chauchau <tesutoero@gmail.com>
2026-02-19 12:20:25 +0900

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,6 @@
 import { auth0 } from "./lib/auth0";
+import type { NextRequest } from "next/server";
 
-export async function middleware(request: Request) {
+export async function middleware(request: NextRequest) {
     return await auth0.middleware(request);
 }
-
-export const config = {
-    matcher: [
-        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"
-    ]
-};
